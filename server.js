3e9136ee529d8c462db7b951bd38d70e658a9013
Lelluz <alelelli.98@gmail.com>
2020-04-19 01:05:03 +0200

    mern server

--- server.js
+++ server.js
@@ -6,13 +6,48 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 8080
 
-mongoose.connect('mongodb://localhost/devs-todos', {
+const MONGODB_URI = 'mongodb+srv://alessandro:capponcelli@devs-todo-tqzha.mongodb.net/test?retryWrites=true&w=majority'
+
+mongoose.connect(MONGODB_URI || 'mongodb://localhost/devs-todos', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 
 mongoose.connection.on('connected', () => {
-  console.log('mongoose is connected!');
+  console.log('mongoose is connected!')
+})
+
+// Schema
+const Schema = mongoose.Schema
+const TaskSchema = new Schema({
+  title: String,
+  jiraCode: String,
+  notes: String,
+  date: {
+    type: String,
+    default: Date.now()
+  }
+})
+
+// Model
+const Task = mongoose.model('Task', TaskSchema)
+
+// Saving data to db
+const data = {
+  title: 'Bug Magic Address',
+  jiraCode: '#2643',
+  notes: 'Sto cercando una soluzione, in quanto alcune parti di codice contengono bug introdotti con un aggiornamento plugin'
+}
+
+// Instance of the model
+const newTask = new Task(data)
+
+newTask.save(error => {
+  if (error) {
+    console.log('Ooops, something happened')
+  } else {
+    console.log('Data has been saved')
+  }
 })
 
 // HTTP request logger
@@ -20,11 +55,15 @@ app.use(morgan('tiny'))
 
 // Routes
 app.get('/api', (req, res) => {
-  const data = {
-    username: 'alessandro',
-    age: 5
-  }
-  res.json(data)
+
+  Task.find({  })
+    .then(data => {
+      res.json(data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+
 })
 
 app.get('/api/name', (req, res) => {
