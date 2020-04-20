6df277c40aa59b64e9c281ed41c6d29584417ae9
lellia <alessandro.lelli.external@ynap.com>
2020-04-21 01:42:40 +0200

    new category button

--- client/src/components/ActionBar.js
+++ client/src/components/ActionBar.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import NewTaskModal from './NewTaskModal'
 import '../styles/ActionBar.scss'
+import NewCategoryModal from './NewCategoryModal';
 
 const ActionBar = () => (
   <nav className="action-bar">
+    <NewCategoryModal></NewCategoryModal>
     <NewTaskModal></NewTaskModal>
   </nav>
 );
